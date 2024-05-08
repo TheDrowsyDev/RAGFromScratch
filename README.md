730888f6c1e7c06b55aab288e631e807c2c4da36
DrowsyDev <vyperius117@gmail.com>
2024-05-07 20:43:05 -0400

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RAGFromScratch
-A Simple RAG Pipeline from Scratch
+
+This repo is a simple exmample of how to run RAG locally with limited GPU resources and no Langchain/Llama index.
+Please refer to the blog posted here: https://medium.com/@vyperius117/rag-from-scratch-with-local-models-b597640b1477
--- src/run_rag_query.py
+++ src/run_rag_query.py
@@ -4,12 +4,17 @@ from openai import OpenAI
 import numpy as np
 from numpy.linalg import norm
 
+# Define URLs for TGI/TEI instances
 rerank_url = "http://localhost:8002"
 tgi_url = "http://localhost:8001"
 tei_url = "http://localhost:8000"
+
+# Parameters for RAG
 threshold = 0.3 # Play around with higher or lower settings!
 max_chunks = 15 # Max chunks for initial cosine search
 rerank_chunks = 5 # Max chunks from the re-ranker
+
+# Prompt to use for final response
 rag_prompt = """
 ### CONTEXT
 {context}
@@ -19,6 +24,8 @@ Query: {query}
 Answer:
 """
 
+# Treat TGI as OpenAI endpoint
+# /v1 on TGI reflects the OpenAI API
 llm = OpenAI(
     api_key="EMPTY",
     base_url=f"{tgi_url}/v1"
@@ -30,7 +37,7 @@ def main(rag_data: list[dict]):
         query = input("Enter a query > ")
         if query == "exit":
             exit(0)
-        
+
         # Vectorize user's original query
         data = {"inputs": [query]}
         response = requests.post(f"{tei_url}/embed", json=data)
@@ -66,6 +73,7 @@ def main(rag_data: list[dict]):
             selected_chunks.append(filtered_context_chunks[idx])
         context = "\n".join(selected_chunks)
 
+        # Pass final formatted prompt with contexts to model
         response = llm.chat.completions.create(
             model="mistral",
             messages=[
@@ -77,15 +85,12 @@ def main(rag_data: list[dict]):
                     )
                 }
             ],
-            stream=True,
+            stream=False,
             max_tokens=1024,
             temperature=0.1
         )
 
-        for elem in response:
-            content = elem.choices[0].delta.content
-            print(content, end="")
-        print("\n")
+        print(f"{response.choices[0].message.content}\n")
     
 
 if __name__ == "__main__":
